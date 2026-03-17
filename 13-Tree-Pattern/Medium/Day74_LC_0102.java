24618be4b23a73cfc1747003c1db2911630e9090
Rifat Ahmed <rifat.ahmed.dev@gmail.com>
2026-03-17 12:08:09 +0600

    Updated LC 0102

--- 13-Tree-Pattern/Medium/Day74_LC_0102.java
+++ 13-Tree-Pattern/Medium/Day74_LC_0102.java
@@ -42,29 +42,37 @@ public class Day74_LC_0102 {
     }
     
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
-        
-        if (root == null) return result;
-        
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        
-        while (!queue.isEmpty()) {
-            int levelSize = queue.size();
-            List<Integer> currentLevel = new ArrayList<>();
-            
-            for (int i = 0; i < levelSize; i++) {
-                TreeNode currentNode = queue.poll();
-                currentLevel.add(currentNode.val);
-                
-                if (currentNode.left != null) queue.offer(currentNode.left);
-                if (currentNode.right != null) queue.offer(currentNode.right);
-            }
-            
-            result.add(currentLevel);
+        List<List<Integer>> res = new ArrayList<>();
+
+        if (root == null) {
+            return res;
         }
-        
-        return result;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            List<Integer> temp = new ArrayList<>();
+            int size = q.size();
+
+            while (size > 0) {
+                TreeNode node = q.peek();
+                q.remove();
+                temp.add(node.val);
+                if (node.left != null) {
+                    q.add(node.left);
+                }
+                if (node.right != null) {
+                    q.add(node.right);
+                }
+
+                size--;
+            }
+
+            res.add(temp);
+        }
+
+        return res;
     }
     
     public static void main(String[] args) {
