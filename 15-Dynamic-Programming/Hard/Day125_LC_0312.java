0764a5e0ba8290ff9fd92f9ae61d1988f25da82d
Rifat Ahmed <rifat.ahmed.dev@gmail.com>
2026-05-09 21:25:54 +0600

    Day 125: Solved LC 312 and wrap-up Dynamic Programming Pattern

---
+++ 15-Dynamic-Programming/Hard/Day125_LC_0312.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Day125_LC_0312 {
+    public static int maxCoins(int[] nums) {
+        int n = nums.length;
+    
+        int[] arr = new int[n + 2];
+        arr[0] = 1;
+        arr[n + 1] = 1;
+        for (int i = 0; i < n; i++) {
+            arr[i + 1] = nums[i];
+        }
+
+        int[][] dp = new int[n + 2][n + 2];
+
+        for (int len = 1; len <= n; len++) {
+            for (int i = 1; i <= n - len + 1; i++) {
+                int j = i + len - 1;
+                
+                for (int k = i; k <= j; k++) {
+                    int coins = arr[i - 1] * arr[k] * arr[j + 1];
+                    int total = coins + dp[i][k - 1] + dp[k + 1][j];
+                    
+                    dp[i][j] = Math.max(dp[i][j], total);
+                }
+            }
+        }
+
+        return dp[1][n];
+    }
+
+    public static void main(String args[]) {
+        int nums[] = {3, 1, 5, 8};
+
+        System.out.println(maxCoins(nums));
+    }
+}
